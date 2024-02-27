531be93305475e48453528a71f801934f4cc8ad8
Lx1Cx <lagunov.2019@list.ru>
2024-02-28 00:35:42 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage/LoginPage.tsx";
+import RegistrationPage from "./pages/auth/RegistrationPage/RegistrationPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage/>
+    },
+    {
+        path: "/registration",
+        element: <RegistrationPage/>
     }
 ])
 
