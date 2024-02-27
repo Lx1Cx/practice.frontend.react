1991b1a4ea17c6f628481a472b21e5b9db033efa
Lx1Cx <lagunov.2019@list.ru>
2024-02-28 01:04:14 +0500

    Вынесена логика в хук

--- src/pages/auth/Form/Form.tsx
+++ src/pages/auth/Form/Form.tsx
@@ -1,19 +1,25 @@
-import {FC} from "react";
+import {FC, FormEvent} from "react";
 import Input from "../../../shared/components/Input/Input.tsx";
 import Button from "../../../shared/components/Button/Button.tsx";
 import classes from "./form.module.css"
+import {IInputEvent} from "../../../shared/components/Input/IInputEvent.ts";
+import {useForm} from "./useForm.ts";
 
 interface IFormProps {
-
+    inputChangeHandler: (data: IInputEvent) => void
+    submitHandler: (event: FormEvent) => void
+    isLoading: boolean
 }
 
-const Form: FC<IFormProps> = ({}) => {
+const Form: FC<IFormProps> = ({inputChangeHandler, submitHandler, isLoading}) => {
+
+    const {SubmitHandler} = useForm(isLoading, submitHandler)
 
     return (
-        <form className={classes.form}>
-            <Input name={"login"} type={"text"} placeholder={"Введите логин"}/>
-            <Input name={"password"} type={"password"} placeholder={"Введите пароль"}/>
-            <Button>Отправить</Button>
+        <form className={classes.form} onSubmit={SubmitHandler}>
+            <Input onChange={inputChangeHandler} name={"login"} type={"text"} placeholder={"Введите логин"}/>
+            <Input onChange={inputChangeHandler} name={"password"} type={"password"} placeholder={"Введите пароль"}/>
+            <Button isLockButton={isLoading}>Отправить</Button>
         </form>
     );
 };
--- src/pages/auth/Form/useForm.ts
+++ src/pages/auth/Form/useForm.ts
@@ -1,6 +1,17 @@
-export const useForm = () => {
+import {FormEvent} from "react";
+
+export const useForm = (isLoading: boolean, onSubmitHandler?: (event: FormEvent) => void) => {
+
+    const SubmitHandler = (event: FormEvent) => {
+        event.preventDefault()
+        if (isLoading) {
+            return
+        }
+
+        onSubmitHandler?.call(null, event)
+    }
 
     return {
-
+        SubmitHandler
     }
 }
