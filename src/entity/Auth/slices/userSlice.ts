3b2815da686c2c069b27bde2962625cb26d3f2c3
Lx1Cx <lagunov.2019@list.ru>
2024-02-28 01:09:17 +0500

    Написан слайс для пользователя

---
+++ src/entity/Auth/slices/userSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface IUser {
+    id: number
+    login: string
+    role: "user" | "admin"
+}
+
+type SliceState = {
+    data: null | IUser
+}
+
+const initialState: SliceState = {
+    data: null
+}
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<IUser>) {
+            state.data = action.payload
+        }
+    }
+})
+
+export const {setUser} = userSlice.actions
