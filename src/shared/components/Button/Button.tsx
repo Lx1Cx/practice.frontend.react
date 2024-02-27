b178c9ab007fb9eaa3797389478c7e05868e3bcc
Lx1Cx <lagunov.2019@list.ru>
2024-02-28 01:03:52 +0500

    Логика блокирования вынесена в отдельный хук

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,20 +1,25 @@
 import {FC, PropsWithChildren} from "react";
 import classes from "./button.module.css"
+import {useButton} from "./useButton.ts";
 
 interface IButtonProps {
     onClick?: () => void
+    isLockButton?: boolean
     styles?: {
         margin?: string
         width?: string
     }
 }
 
-const Button: FC<PropsWithChildren<IButtonProps>> = ({children, styles, onClick}) => {
+const Button: FC<PropsWithChildren<IButtonProps>> = ({children, styles, onClick, isLockButton}) => {
+
+    const {ClickHandler} = useButton(isLockButton, onClick)
+
     return (
         <button
-            onClick={onClick}
+            onClick={ClickHandler}
             style={styles}
-            className={classes.button}
+            className={isLockButton ? classes.button_lock : classes.button}
         >
             {children}
         </button>
