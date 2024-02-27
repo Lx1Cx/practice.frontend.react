4c508a4d6f2b1c34e7b43b2f2f2b3d8fa70c5844
Lx1Cx <lagunov.2019@list.ru>
2024-02-28 00:36:08 +0500

    Добавлены провайдеры

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
+import {Provider} from "react-redux";
+import {persistor, store} from "./store.ts";
+import {PersistGate} from "redux-persist/integration/react";
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <App />
+            <ToastContainer/>
+        </PersistGate>
+    </Provider>
 )
