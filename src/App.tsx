93b7c6c96f765eb9cad4c0687b4b3202e2839e5b
lx1cx <lagunov.2019@list.ru>
2024-02-28 17:59:02 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage/LoginPage.tsx";
 import RegistrationPage from "./pages/auth/RegistrationPage/RegistrationPage.tsx";
+import MainAdminPage from "./pages/admin/MainAdminPage/MainAdminPage.tsx";
+import MainPlacesPage from "./pages/admin/places/MainPlacesPage/MainPlacesPage.tsx";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,26 @@ const router = createBrowserRouter([
     {
         path: "/registration",
         element: <RegistrationPage/>
+    },
+
+    {
+        path: "admin",
+        children: [
+            {
+                path: "",
+                element: <MainAdminPage/>
+            },
+
+            {
+                path: "places",
+                children: [
+                    {
+                        path: "",
+                        element: <MainPlacesPage/>
+                    }
+                ]
+            }
+        ]
     }
 ])
 
