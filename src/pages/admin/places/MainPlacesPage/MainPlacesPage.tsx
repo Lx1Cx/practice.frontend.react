1da0655f6d87979e608a16a9b86f2c87c3d7d930
lx1cx <lagunov.2019@list.ru>
2024-02-28 18:00:44 +0500

    Create main page for places

---
+++ src/pages/admin/places/MainPlacesPage/MainPlacesPage.tsx
@@ -0,0 +1,16 @@
+import classes from "./mainPlacesPage.module.css"
+import Button from "../../../../shared/components/Button/Button.tsx";
+
+const MainPlacesPage = () => {
+    return (
+        <div className={classes.wrapper}>
+            <Button>Create new place</Button>
+
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default MainPlacesPage;
