70fa1fd074e85e66d74dec581422f1515e6a7189
lx1cx <lagunov.2019@list.ru>
2024-02-28 18:00:28 +0500

    Create main page for admin panel

---
+++ src/pages/admin/MainAdminPage/MainAdminPage.tsx
@@ -0,0 +1,15 @@
+import classes from "./mainAdminPage.module.css"
+import MainAdminPageItem from "./UI/MainAdminPageItem/MainAdminPageItem.tsx";
+
+const MainAdminPage = () => {
+    return (
+        <div className={classes.wrapper}>
+            <h1>Admin panel</h1>
+            <div className={classes.modal}>
+                <MainAdminPageItem text={"Place"} href={"places"}/>
+            </div>
+        </div>
+    );
+};
+
+export default MainAdminPage;
