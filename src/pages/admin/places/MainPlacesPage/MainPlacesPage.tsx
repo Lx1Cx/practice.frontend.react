a9a9685787452f266cc6103da404956747765489
Lx1Cx <lagunov.2019@list.ru>
2024-02-29 13:55:35 +0500

    Сделана главная страница для мест в админке

--- src/pages/admin/places/MainPlacesPage/MainPlacesPage.tsx
+++ src/pages/admin/places/MainPlacesPage/MainPlacesPage.tsx
@@ -1,13 +1,36 @@
 import classes from "./mainPlacesPage.module.css"
 import Button from "../../../../shared/components/Button/Button.tsx";
+import {useMainPlacesPage} from "./useMainPlacesPage.ts";
+import PlaceCard from "../../../../shared/components/PlaceCard/PlaceCard.tsx";
+import AdminItemWrapper from "../../UI/AdminWrapper/AdminItemWrapper.tsx";
 
 const MainPlacesPage = () => {
+
+    const {navigate, getAllPlacesResult, isLoading} = useMainPlacesPage()
+
+    //todo: Change this
+
+    if (isLoading || !getAllPlacesResult) {
+        return <p>Loading</p>
+    }
+
     return (
         <div className={classes.wrapper}>
-            <Button>Create new place</Button>
+            <Button onClick={() => navigate("create")}>Создать новое место</Button>
 
-            <div>
-
+            <div className={classes.places}>
+                {getAllPlacesResult.length === 0
+                    ? <p>Здесь пока ничего нет</p>
+                    : getAllPlacesResult.map(place => (
+                        <AdminItemWrapper>
+                            <PlaceCard
+                                key={place.id}
+                                name={place.name}
+                                description={place.description}
+                            />
+                        </AdminItemWrapper>
+                    ))
+                }
             </div>
         </div>
     );
--- src/pages/admin/places/MainPlacesPage/useMainPlacesPage.ts
+++ src/pages/admin/places/MainPlacesPage/useMainPlacesPage.ts
@@ -1,8 +1,23 @@
+import {useNavigate} from "react-router-dom";
+import {useGetAllPlacesQuery} from "../../../../entity/Place/api/PlaceApi.ts";
+import {useEffect} from "react";
+import {AlertService} from "../../../../shared/services/AlertService.ts";
+
 export const useMainPlacesPage = () => {
 
+    const {data: getAllPlacesResult , isLoading, error}  = useGetAllPlacesQuery()
 
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (error && "data" in error) {
+            return AlertService.error(error.data.displayMessage)
+        }
+    }, [error]);
 
     return {
-
+        getAllPlacesResult,
+        navigate,
+        isLoading
     }
 }
