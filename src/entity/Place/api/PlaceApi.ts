c07c688e0a76ceee94c2630879cfd49b9fcd9339
Lx1Cx <lagunov.2019@list.ru>
2024-02-29 13:56:27 +0500

    Написан слайс для взаимодействия с местами

--- src/entity/Place/api/PlaceApi.ts
+++ src/entity/Place/api/PlaceApi.ts
@@ -2,8 +2,13 @@ import {BaseQueryFn, createApi, FetchArgs, fetchBaseQuery} from "@reduxjs/toolki
 import {BASE_API_URL} from "../../../consts.ts";
 import {IBaseErrorResponse} from "../../../shared/models/IBaseErrorResponse.ts";
 import {IGetPlacesResponse} from "../models/responses/IGetPlacesResponse.ts";
+import {ICreatePlaceRequest} from "../models/requests/ICreatePlaceRequest.ts";
+import {ICreatePlaceResponse} from "../models/responses/ICreatePlaceResponse.ts";
+import {IUpdatePlaceResponse} from "../models/responses/IUpdatePlaceResponse.ts";
+import {IUpdatePlaceRequest} from "../models/requests/IUpdatePlaceRequest.ts";
+import {TokenService} from "../../../shared/services/TokenService.ts";
 
-const placeApi = createApi({
+export const placeApi = createApi({
     reducerPath: "place",
     baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }) as BaseQueryFn<
         string | FetchArgs,
@@ -17,17 +22,56 @@ const placeApi = createApi({
                 url: "/tour-places",
                 method: "GET",
             }),
-            providesTags: ["place"]
+            providesTags: (result) => result
+                ? [
+                    ...result.map(({id}) => ({type: "place" as const, id})),
+                    {type: "place", id: "LIST"}
+                ]
+                : [ {type: "place", id: "LIST"}],
         }),
 
         getPlaceById: build.query<IGetPlacesResponse, number>({
             query: (id) => ({
-                url: `/tour-place/${id}`,
+                url: `/tour-places/${id}`,
                 method: "GET",
             }),
-            providesTags: ["place"]
+            providesTags: (_, __, id) => [{ type: 'place' as const, id }],
+        }),
+
+        createPlace: build.mutation<ICreatePlaceResponse, ICreatePlaceRequest>({
+            query: (body) => ({
+                url: "/tour-places",
+                method: "POST",
+                body,
+                headers: {
+                    Authorization: `Bearer ${TokenService.getAccessToken()}`
+                }
+            }),
+            invalidatesTags:  [{ type: 'place', id: 'LIST' }]
+        }),
+
+        updatePlaceById: build.mutation<IUpdatePlaceResponse, IUpdatePlaceRequest>({
+            query: ({id, ...body}) => ({
+                url: `/tour-place/${id}`,
+                method: "PUT",
+                body,
+                headers: {
+                    Authorization: `Bearer ${TokenService.getAccessToken()}`
+                }
+            })
+        }),
+
+        deletePlaceById: build.mutation<void, number>({
+            query: (id) => ({
+                url: `/tour-places/${id}`,
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${TokenService.getAccessToken()}`
+                }
+            }),
+            invalidatesTags: [{type: "place", id: "LIST"}]
         })
     })
 })
 
-export const {useGetAllPlacesQuery} = placeApi
+export const {useGetAllPlacesQuery, useDeletePlaceByIdMutation, useCreatePlaceMutation, useGetPlaceByIdQuery, useUpdatePlaceByIdMutation} = placeApi
