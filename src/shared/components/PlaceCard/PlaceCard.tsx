4111564d1506422580f23c23a81d0b945f8c1886
lx1cx <lagunov.2019@list.ru>
2024-03-01 17:57:01 +0500

    add image to background

--- src/shared/components/PlaceCard/PlaceCard.tsx
+++ src/shared/components/PlaceCard/PlaceCard.tsx
@@ -1,17 +1,21 @@
 import {FC} from "react";
 import classes from "./placeCard.module.css"
+import {BASE_API_URL} from "../../../consts.ts";
 
 interface IPlaceCardProps {
+    image?: string
     name: string
     description: string
 }
 
-const PlaceCard: FC<IPlaceCardProps> = ({name, description}) => {
+const PlaceCard: FC<IPlaceCardProps> = ({name, description, image}) => {
     return (
         <div className={classes.card}>
-            <div className={classes.image}>
-                <p>Здесь пока нет картинки</p>
-            </div>
+            {image
+                ? <div style={{backgroundImage: `url(${BASE_API_URL}/${image})`}} className={classes.image}></div>
+                : <div className={classes.image}>
+                    <p>Здесь пока нет картинки</p>
+                </div>}
 
             <div className={classes.info}>
                 <p title={name} className={classes.caption}>{name}</p>
