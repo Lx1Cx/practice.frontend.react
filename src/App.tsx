bd54b0dfdd21cef34a081ada6b7b779b205cfc80
lx1cx <lagunov.2019@list.ru>
2024-03-01 17:55:45 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import RegistrationPage from "./pages/auth/RegistrationPage/RegistrationPage.tsx
 import MainAdminPage from "./pages/admin/MainAdminPage/MainAdminPage.tsx";
 import MainPlacesPage from "./pages/admin/places/MainPlacesPage/MainPlacesPage.tsx";
 import CreatePlacePage from "./pages/admin/places/CreatePlacePage/CreatePlacePage.tsx";
+import MainToursPage from "./pages/admin/tours/MainToursPage/MainToursPage.tsx";
+import CreateTourPage from "./pages/admin/tours/CreateTourPage/CreateTourPage.tsx";
 
 const router = createBrowserRouter([
     {
@@ -34,6 +36,21 @@ const router = createBrowserRouter([
                     {
                         path: "create",
                         element: <CreatePlacePage/>
+                    }
+                ]
+            },
+
+            {
+                path: "tours",
+                children: [
+                    {
+                        path: "",
+                        element: <MainToursPage/>
+                    },
+
+                    {
+                        path: "create",
+                        element: <CreateTourPage/>
                     }
                 ]
             }
